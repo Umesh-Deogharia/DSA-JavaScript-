bbd3346b22c38a958786309c776226b8214e2720
Umesh-Deogharia <umeshdeogharia07@gmail.com>
2025-09-06 09:27:43 +0530

    A Small Optimization on the code to prevent  the number is swapping itself on the same position

--- Searching&Sorting/Selection Sort/selectionSort.js
+++ Searching&Sorting/Selection Sort/selectionSort.js
@@ -1,19 +1,21 @@
 let arr = [1, 3, 2, 4, 2, 4, 5, 6, 4, 8, 0];
 
-for (let i = 0; i < arr.length-1; i++){
+for (let i = 0; i < arr.length - 1; i++) {
     let min = i;
-    for (let j = i+1; j < arr.length; j++) {
-        if (arr[min]>arr[j]) {
+    for (let j = i + 1; j < arr.length; j++) {
+        if (arr[min] > arr[j]) {
             min = j;
         }
     }
-    let temp = arr[min];
-    arr[min] = arr[i];
-    arr[i] = temp;
+    if (min != i) {
+        let temp = arr[min];
+        arr[min] = arr[i];
+        arr[i] = temp;
+    }
 }
 
-console.log(arr)
-                                                //  or
+console.log(arr);
+//  or
 
 // let arr = [1, 3, 2, 4, 2, 4, 5, 6, 4, 8, 0];
 
