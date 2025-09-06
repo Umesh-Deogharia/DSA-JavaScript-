ac538e43f2517a831acf5004d77f790ed1d4621b
Umesh-Deogharia <umeshdeogharia07@gmail.com>
2025-09-06 09:21:46 +0530

    Two Version Of code only a small changes on both of the code inside i and min and j

--- Searching&Sorting/Selection Sort/selectionSort.js
+++ Searching&Sorting/Selection Sort/selectionSort.js
@@ -1,8 +1,8 @@
 let arr = [1, 3, 2, 4, 2, 4, 5, 6, 4, 8, 0];
 
-for (let i = 0; i < arr.length; i++){
-    let min = 0;
-    for (let j = i+1; j < arr.length-1; j++) {
+for (let i = 0; i < arr.length-1; i++){
+    let min = i;
+    for (let j = i+1; j < arr.length; j++) {
         if (arr[min]>arr[j]) {
             min = j;
         }
@@ -11,4 +11,21 @@ for (let i = 0; i < arr.length; i++){
     arr[min] = arr[i];
     arr[i] = temp;
 }
+
 console.log(arr)
+                                                //  or
+
+// let arr = [1, 3, 2, 4, 2, 4, 5, 6, 4, 8, 0];
+
+// for (let i = 0; i < arr.length; i++) {
+//     let min = 0;
+//     for (let j = i + 1; j < arr.length - 1; j++) {
+//         if (arr[min] > arr[j]) {
+//             min = j;
+//         }
+//     }
+//     let temp = arr[min];
+//     arr[min] = arr[i];
+//     arr[i] = temp;
+// }
+// console.log(arr)
---
+++ Searching&Sorting/Selection Sort/tempCodeRunnerFile.js
@@ -0,0 +1,15 @@
+
+let arr = [1, 3, 2, 4, 2, 4, 5, 6, 4, 8, 0];
+
+for (let i = 0; i < arr.length; i++) {
+    let min = 0;
+    for (let j = i + 1; j < arr.length - 1; j++) {
+        if (arr[min] > arr[j]) {
+            min = j;
+        }
+    }
+    let temp = arr[min];
+    arr[min] = arr[i];
+    arr[i] = temp;
+}
+console.log(arr)
